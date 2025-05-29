ea7a2b8144235407e2a147887ec98fc8c23b6162
omidfa <omidfa1234@gmail.com>
2025-05-29 11:53:26 +0330

    Add listings page

--- js/app.js
+++ js/app.js
@@ -1,10 +1,44 @@
-// console.log("omid");
-// const searchParams = new URLSearchParams(window.location.search);
+const BASE_URL = "https://www.omidfaryabi.ir/api";
 
-// const city = searchParams.get("city"),
-//   building = searchParams.get("building"),
-//   budget = searchParams.get("budget"),
-//   room = searchParams.get("room");
+const switchBtn = document.getElementById("switch");
+
+//  <!-- Enabled: "", Not Enabled: "bg-gray-200" -->
+
+switchBtn.addEventListener("click", () => {
+  switchBtn.classList.toggle("bg-red-400");
+  switchBtn.children.item(1).classList.toggle("translate-x-5");
+});
+
+console.log("s", switchBtn);
+
+const searchParams = new URLSearchParams(window.location.search);
+
+const city = searchParams.get("city"),
+  building = searchParams.get("building"),
+  budget = searchParams.get("budget"),
+  room = searchParams.get("room");
+
+const getListingData = async (prams) => {
+  const url = new URLSearchParams(BASE_URL + "/listing");
+
+  // for (const key in prams) {
+  //   if (Object.prototype.hasOwnProperty.call(prams, key)) {
+  //     const element = prams[key];
+  //     url.append(key, element);
+  //   }
+  // }
+
+  console.log(BASE_URL + "/listing");
+  const request = await fetch(BASE_URL + "/listings", {
+    method: "GET",
+  });
+
+  const response = await request.json();
+
+  console.log(response);
+};
+
+getListingData({ city, building, budget, room });
 
 // fetch(url, {
 //   method: "GET",
